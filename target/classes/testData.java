83aec2aa9e34202a7a025d07eb5693fcf2700e1e
Lat Caesar <lattrell.caesar-cc@bregroup.com>
2025-03-07 21:07:59 +0800

    initial commit to repo

---
+++ target/classes/testData.java
@@ -0,0 +1,26 @@
+
+public class testData {
+
+	public static String BreOrg = "Automation Organization  Do not delete";
+    public static String BreAsset = "Automation Asset Don not DELETE";
+    public static String BreAssessment = "Automation Assessment Do not DELETE";
+    public static String BreAssessment1 = "Automation Asset Don not DELETE 1";
+    public static String BreQAAssessment = "Automation Resubmission Asset";
+    
+    //Scheme basis PRE-PROD - https://portal-preprod.breeam.com/schemerevision/detail?menuIndex=5&schemecode=NEWCON&schemeedition=AutomationUserONLY&version=1&revisionversion=1&
+    public static String BREscheme = "AutomationUserONLY"; // Use to validate issue credits
+    public static String BREexemplaryAssessment = "AutomationUserONLY";// Use to validate Exemplary Credits
+   
+    public static String BreSchemeRevisionName = "zAuto Test Scheme AssertName01";
+
+    public static String userAdmin = "UATuser Regression";
+    public static String userDashboardAdmin = "UATuser Regression";
+    public static String userTechnicalSupport = "Michelle Cortes";
+    public static String userSeniorTechnicalSupport = "Allan Robles";
+    public static String userOrgAdmin = "Aditi Jain";
+    public static String userDashboardManager = "Aditi Jain";
+    public static String userAssessmentStakeHolder = "Adam Winser";
+    public static String userAssessmentManager = "Aaron Dixey";
+    public static String userAssessmentContributor = "Taylor Hudson";
+    public static String userAssessmentAdmin = "Michelle Cortes";
+}
